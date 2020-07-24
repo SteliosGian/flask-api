efb68cda633c1efe02e9608b402250dc7cc1e522
Giannikis <giannikiss@ryanair.com>
2020-07-24 08:44:56 +0100

    Minor changes and additions

--- app.py
+++ app.py
@@ -12,10 +12,11 @@ from resources.store import Store, StoreList
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'stelios'
 api = Api(app)
 
-jwt = JWT(app, authenticate, identity) #/auth endpoint
+jwt = JWT(app, authenticate, identity)  # /auth endpoint
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
