fde96a95766aebdaa66a1a9f5a9e453d91195f99
Giannikis <steliosgiannik@gmail.com>
2020-07-24 10:34:34 +0100

    Add JWT claims

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
+from db import db
 
 from resources.user import UserRegister, User, UserLogin
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'stelios'
@@ -17,6 +19,17 @@ app.secret_key = 'stelios'
 api = Api(app)
 
 jwt = JWTManager(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+@jwt.user_claims_loader
+def add_claims_to_jwt(identity):
+    if identity == 1:
+        return {'is_admin': True}
+    return {'is_admin': False}
+
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
