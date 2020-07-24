446607d1bbf251209d2f5b21499cdc427ed368bb
Giannikis <steliosgiannik@gmail.com>
2020-07-24 11:37:07 +0100

    Improved blacklist

--- app.py
+++ app.py
@@ -5,14 +5,17 @@ from flask_restful import Api
 from flask_jwt_extended import JWTManager
 from db import db
 
-from resources.user import UserRegister, User, UserLogin, TokenRefresh
+from resources.user import UserRegister, User, UserLogin, UserLogout, TokenRefresh
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+from blacklist import BLACKLIST
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 app.secret_key = 'stelios'
 # app.config['JWT_SECRET_KEY'] = True
 api = Api(app)
@@ -30,6 +33,11 @@ def add_claims_to_jwt(identity):
     if identity == 1:
         return {'is_admin': True}
     return {'is_admin': False}
+
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    return decrypted_token['jti'] in BLACKLIST
 
 
 @jwt.expired_token_loader
@@ -79,9 +87,10 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(UserLogin, '/login')
+api.add_resource(UserLogout, '/logout')
 api.add_resource(TokenRefresh, '/refresh')
 
 if __name__ == '__main__':
-    from db import db
+    #from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
